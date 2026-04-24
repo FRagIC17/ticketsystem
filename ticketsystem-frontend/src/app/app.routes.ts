2d38d6523485c70514d09422c081fe430b6dd72b
Mie Damgaard <mie@Mies-MacBook-Pro.local>
2026-04-24 09:02:52 +0200

    created routing and navigation for all pages

--- ticketsystem-frontend/src/app/app.routes.ts
+++ ticketsystem-frontend/src/app/app.routes.ts
@@ -1,6 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
+import { Ticketboard } from './components/dashboard/ticketboard/ticketboard';
+import { NgModule } from '@angular/core';
+import { Faqpage } from './components/dashboard/faqpage/faqpage';
+import { Createticket } from './components/dashboard/ticketboard/createticket/createticket';
 
 export const routes: Routes = [
-    {path: '', component: Dashboard}
+    {path: '', component: Dashboard},
+    {path: 'my-tickets', component: Ticketboard},
+    {path: 'faq', component: Faqpage},
+    {path: 'create-ticket', component: Createticket}
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- ticketsystem-frontend/src/app/components/navbar/navbar.ts
+++ ticketsystem-frontend/src/app/components/navbar/navbar.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  constructor(private http: HttpClient, private router: Router) { }
+
+  myDashboard() {
+    this.router.navigate(['/']);
+  }
+
+  myTickets() {
+    this.router.navigate(['/my-tickets']);
+  }
+
+  myUsers() {
+    this.router.navigate(['/my-users']);
+  }
+
+  mySettings() {
+    this.router.navigate(['/my-settings']);
+  }
+
+  myFAQ() {
+    // Implement navigation to FAQ page
+    this.router.navigate(['/faq']);
+  }
+}
