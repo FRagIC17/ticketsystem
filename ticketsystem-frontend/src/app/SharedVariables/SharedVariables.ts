284de08d66c83e2fd8ca0054bf646328fb3633f2
Mie Damgaard <mie@Mies-MacBook-Pro.local>
2026-04-24 09:04:09 +0200

    updated html on dashboard + baseurl for backend

---
+++ ticketsystem-frontend/src/app/SharedVariables/SharedVariables.ts
@@ -0,0 +1,5 @@
+export class SharedVariables {
+    public static baseUrl: string = 'http://localhost:8080';
+
+    
+}
--- ticketsystem-frontend/src/app/components/dashboard/dashboard.ts
+++ ticketsystem-frontend/src/app/components/dashboard/dashboard.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { Ticketboard } from './ticketboard/ticketboard';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Navbar],
+  imports: [Navbar, CommonModule, HttpClientModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
-  
+  amountOfTickets: number = 0;
+
+  constructor(private Ticketboard: Ticketboard) { }
+
+  ngOnInit() {
+    this.findAmountOfTickets();
+  }
+
+  findAmountOfTickets() {
+    this.amountOfTickets = this.Ticketboard.getTickets().length;
+  }
+
+  onTicketClick(ticket: any) {
+
+  }
 }
