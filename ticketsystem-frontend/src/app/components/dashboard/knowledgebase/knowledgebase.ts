b6b6ae96f4250d3c7878ce06035e1ef25ab08c10
Philip <Philipmds@icloud.com>
2026-04-28 13:47:18 +0200

    part 2 of moving knowledgebase and createticket component away from dashboard

--- ticketsystem-frontend/src/app/components/dashboard/knowledgebase/knowledgebase.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-knowledgebase',
-  imports: [],
-  templateUrl: './knowledgebase.html',
-  styleUrl: './knowledgebase.css',
-})
-export class Knowledgebase {}
