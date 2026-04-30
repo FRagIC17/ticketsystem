a33759565d9ad6eaa514ef302e523f528d46868c
Philip <Philipmds@icloud.com>
2026-04-30 12:55:50 +0200

    made contact html, maybe add contact form later

--- ticketsystem-frontend/src/app/components/contact/contact.ts
+++ ticketsystem-frontend/src/app/components/contact/contact.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID, Signal, signal } from '@angular/core';
+import { SharedVariables } from '../../SharedVariables/SharedVariables';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class Contact {}
+export class Contact {
+
+  itSupporters = signal<any[]>([]);
+
+  private http = inject(HttpClient);
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+    this.getItSupporters();
+    }
+  }
+
+  getItSupporters() {
+    this.http.get<any[]>(`${SharedVariables.baseUrl}/api/it-supporters`).subscribe({
+      next: (data) => {
+        this.itSupporters.set(data);
+      },
+      error: (err) => {
+        console.error('Error fetching IT supporters:', err);
+        this.itSupporters.set([]);
+      }
+    });
+  }
+}
